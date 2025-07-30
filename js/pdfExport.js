1a4ccd98b1f5b05ec7c4dd786e933c0c5316f528
Татьяна Луганцева <lgntsva.t@yandex.ru>
2025-07-30 16:18:37 +0300

    replace icons

--- js/pdfExport.js
+++ js/pdfExport.js
@@ -13,6 +13,7 @@ export const pdfExport = {
     const canvas = await html2canvas(resumeElement, {
       scale: 6,
       useCORS: true,
+      svgRendering: true,
       windowWidth: 1440,
     });
 
